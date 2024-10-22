7f6fa29e67c10d36ddd7b69928168307d0fa12df
Dmitrii Temin <t.dmitrii@postilla.ru>
2024-10-22 12:53:27 +0300

    subpackage @mqb/libs linked

--- apps/notifier/src/app.service.ts
+++ apps/notifier/src/app.service.ts
@@ -1,15 +1,10 @@
-import {
-  BadRequestException,
-  HttpException,
-  Injectable,
-  UseFilters,
-} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 import { AxiosError } from 'axios';
 import { log } from 'console';
 import { catchError, EMPTY, firstValueFrom, ObservableInput, of } from 'rxjs';
 import { HttpService } from '@nestjs/axios';
-import { Message } from '../../../libs/message.interface';
+import { Message } from '@mqb/libs/src/message.interface';
 
 @Injectable()
 export class NotificationService {
@@ -20,7 +15,7 @@ export class NotificationService {
   async notify(message: Message): Promise<any> {
     const { data: response } = await firstValueFrom(
       this.http.post(this.receiverUrl, message).pipe(
-        catchError((error: AxiosError<{ message: string }>) => {
+        catchError((error: Required<Pick<AxiosError<any>, 'response'>>) => {
           throw new RpcException(
             `${message.id} - !ERROR: Receiver responded ${error.response.data.message}`,
           );
